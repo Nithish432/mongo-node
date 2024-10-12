655d42600c86b11d216c96443efa3d74a1eff07a
Nithish kumar K <knithish432@gmail.com>
2024-10-13 01:26:37 +0530

    removed unwanted files

--- services/users.service.js
+++
@@ -1,9 +0,0 @@
-import { client } from "../index.js";
-
-export async function createUser(data) {
-    return await client.db("newdb").collection("users").insertOne(data);
-}
-
-export async function getUserByName(username){
-    return await client.db("newdb").collection("users").findOne({username: username})
-}
